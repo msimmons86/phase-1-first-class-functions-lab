1923c4c1cd443b2af3cfbd02655160d3ee2286d0
Michael Simmons <msimmons86@gmail.com>
2022-03-24 16:53:54 -0700

    commit tree e8b9c0f19e40b890e90d1bf4656fa4114938171b

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers = bob => [...bob.slice(0, 2)]
+
+const returnLastTwoDrivers = drivers => [...drivers.slice(2, 4)]
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(x) {
+  return function (timesFour) {
+  return x * timesFour
+  }
+}
+
+const fareDoubler = money => money*2
+const fareTripler = money => money*3
+
+
+function selectDifferentDrivers(drivers, selectingDrivers) {
+  return selectingDrivers(drivers)
+}
